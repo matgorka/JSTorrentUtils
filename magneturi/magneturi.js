24c17f344ff8d4f2c538b3ba1957368a36824619
Mateusz Górka <119092143+matgorka@users.noreply.github.com>
2023-02-08 03:14:24 +0100

    magneturi: so parameter implemented

--- magneturi/magneturi.js
+++ magneturi/magneturi.js
@@ -1,11 +1,13 @@
 const xt = uri => /^urn:([A-Z0-9]+(?::+[A-Z0-9]+)*:*):/i.exec(uri)[1];
 
-const rawPush = (o, key, value) =>
-  o[key] ? o[key].push(value) : o[key] = [ value ];
-
-const push = (o, key, value) => {
-  let usedProtocols = [],
-      protocol,
+const set = (o, key, value) => {
+  let usedProtocols = [], // for xt
+      protocol,           // for xt
+      bitfield      = [], // for so
+      arr           = [], // for so
+      range,              // for so
+      l,                  // for so
+      r,                  // for so
       i;
 
   try {
@@ -20,7 +22,7 @@ const push = (o, key, value) => {
         o[key] = [ value + "" ];
         return;
 
-      /* repeatable parameters combined into single one */
+      /* repeatable parameters combinable into single ones */
       case "kt":
         if (!o.kt)
           o.kt = [ "" ];
@@ -32,10 +34,41 @@ const push = (o, key, value) => {
         return;
 
       case "so":
-        // TO DO!
+        if (!o.so)
+          o.so = [ "" ];
+
+        for (range of (o.so[0] + "," + value).split(",")) {
+          if (!range || !/^\d+(?:-\d+)?$/.exec(range))
+            continue;
+
+          range = range.split("-");
+          l     = range[0] * 1;
+          r     = range[1] || range[0];
+
+          if (l > r)
+            [r, l] = [l, r];
+
+          for (i = l; i <= Math.min(r, 1e6); ++i)
+            bitfield[i] = 1;
+        }
+
+        for (l = -1, r = 0; r <= bitfield.length; ++r) {
+          if (bitfield[r]) {
+            if (l < 0)
+              l = r;
+          } else if (l >= 0) {
+            if (r - l > 1)
+              l += "-" + (r - 1);
+
+            arr.push(l);
+            l = -1;
+          }
+        }
+
+        o.so = [ arr.join() ];
         return;
 
-      /* XT */
+      /* repeatable but once per protocol: xt parameter */
       case "xt":
         try {
           usedProtocols = o.xt.map(uri => xt(uri))
@@ -53,7 +86,10 @@ const push = (o, key, value) => {
         break;
     }
 
-    rawPush(o, key, value);
+    if (o[key])
+      o[key].push(value)
+    else
+      o[key] = [ value ];
   } catch(err) {
   }
 };
@@ -63,8 +99,10 @@ function MagnetURI(uri) {
     /^magnet\:\?(.*)/.exec(uri)[1]
       .split("&")
       .map(param => param.split("=").map(x => decodeURIComponent(x)))
-      .forEach(([key, value]) => push(this.params, key, value));
+      .forEach(([key, value]) => set(this.params, key, value));
   } catch(err) {
     throw new Error("Invalid magnet uri.");
   }
 }
+
+var x={};
