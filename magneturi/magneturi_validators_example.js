085748dcd2c6ae85ebaa9aac496e6fa59e6e1560
Mateusz Górka <119092143+matgorka@users.noreply.github.com>
2023-02-15 10:10:05 +0100

    example validator

--- magneturi/magneturi_validators_example.js
+++ magneturi/magneturi_validators_example.js
@@ -3,18 +3,11 @@ let validateXT = (key, [protocol, hash]) => {
   if (key != "xt")
     return 1;
 
-  switch (protocol) {
-    case "btmh":
-    case "btih":
-      if (/[^A-F\d]/i.test(hash))
-        return 0;
-  }
-
   if (protocol == "btmh")
-    return hash.length == 64;
+    return hash.length == 64 && !/[^A-F\d]/i.test(hash);
 
   if (protocol == "btih")
-    return hash.length == 40;
+    return hash.length == 40 && !/[^A-F\d]/i.test(hash);
 
   return 1;
 };
